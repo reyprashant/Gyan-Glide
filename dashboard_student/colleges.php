445c627fad004656a47eb03fb6b0bbb199ae4682
Sushan Poudel <sushanpoudel75@gmail.com>
2024-06-25 12:46:49 +0545

    bugs fixed

--- dashboard_student/colleges.php
+++ dashboard_student/colleges.php
@@ -15,36 +15,40 @@ $result = $conn->query($sql);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/all.min.css"> 
+    <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/framework.css">
     <link rel="stylesheet" href="css/normalize.css">
-     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Rubik:wght@300;400;600;900&family=Work+Sans:wght@300;400;500;600;800&display=swap" rel="stylesheet">
     <title>Colleges</title>
 </head>
+
 <body>
 
-<?php
- while ($colleges_row = mysqli_fetch_assoc($result)) {}
-?>
+    <?php
+    while ($colleges_row = mysqli_fetch_assoc($result)) {
+    }
+    ?>
     <div class="courses page d-flex">
         <?php
-        require_once'dashboard_navbar.php';
+        require_once 'dashboard_navbar.php';
         ?>
-        
+
         <div class="content d-flex  column">
-        <?php
-        require_once'dashboard_header.php';
-        ?>
+            <?php
+            require_once 'dashboard_header.php';
+            ?>
             <h1 class="p-relative mt-10">Colleges</h1>
-            
+
         </div>
     </div>
 
 </body>
+
 </html>
