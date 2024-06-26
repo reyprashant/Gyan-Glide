150636ff0004c3481611dfacefd8c697adb0b734
Akriti Dhakal <136664101+akritidhakal@users.noreply.github.com>
2024-06-26 12:11:34 +0545

    footer_bar added

---
+++ footer_bar.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<script src="script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+</body>
+
+<footer>
+    <div class="footerContainer">
+        <div style="display: flex; align-items: center; justify-content: center;">
+            <img src="images/whitelogo.png" alt="" style="object-fit: cover; width: 250px; height: 150px; margin-left: 20px;">
+        </div>
+        <div class="footerNav">
+                <div><h3 style="color: white; text-align: center; margin-bottom: 20px;">Contact us</h1>
+                    <p style="color: whitesmoke;margin-bottom: 10px;"><i class="fa-solid fa-phone"></i>&nbsp;&nbsp;&nbsp;&nbsp; 061-521165</p>
+                    <p style="color: whitesmoke;margin-bottom: 10px;"><i class="fa-solid fa-phone"></i>&nbsp;&nbsp;&nbsp;&nbsp; +977 9832753574</p>
+                    <p style="color: whitesmoke;margin-bottom: 10px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;"><i class="fa-regular fa-envelope"></i>&nbsp;&nbsp;&nbsp; gyanglide24@gmail.com</p>
+                    <p style="color: whitesmoke;"></p>
+                </div>
+                <div style="margin-right: 70px;"><h3 style="color: white; text-align:center; margin-bottom: 20px;">Follow Us</h3>
+                <div class="socialIcons">
+            <a href=""><i class="fa-brands fa-facebook"></i></a>
+            <a href=""><i class="fa-brands fa-instagram"></i></a>
+            <a href=""><i class="fa-brands fa-github"></i></a>
+            <a href=""><i class="fa-brands fa-twitter"></i></a>
+            <a href=""><i class="fa-brands fa-linkedin"></i></a>
+        </div>
+                </div>
+                <div><h3 style="color: white; text-align:center; margin-bottom: 20px;">Our Team</h3>
+                    <p style="color: whitesmoke;">Prashant Adhikari <br> Akriti Dhakal <br> Sushan Poudel <br> Suraj Sapkota</p>
+                </div>
+        </div>
+        <div style="display: flex; align-items: center; justify-content: center; border-top: 1px solid white; color: white; padding: 20px;">
+            <i class="fa-regular fa-copyright"></i>&nbsp; &nbsp; Gyan-Glide,&nbsp; All Rights Reserved
+        </div>
+        </div>
+    </div>
+</footer>
+
+</html>
+
