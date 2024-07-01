cabb5c6615fbeaabe44dfcc16320eafb9a7cc758
Sushan Poudel <sushanpoudel75@gmail.com>
2024-07-01 11:23:09 +0545

    image fetch from db succes

---
+++ image_upload/abc.php
@@ -0,0 +1,52 @@
+<?php 
+
+    # database connection file
+	include '../connectionSetup.php';
+
+	# fetching images
+	$sql  = "SELECT img_name FROM
+	         college_images ORDER BY img_name DESC";
+
+	$stmt = $conn->prepare($sql);
+	$stmt->execute();
+    $result = $stmt->get_result();
+    $images = $result->fetch_all(MYSQLI_ASSOC);
+print_r($images);
+
+ ?> 
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" 
+	      action="upload.php"
+	      enctype="multipart/form-data">
+          <?php  
+            if (isset($_GET['error'])) {
+            	echo "<p class='error'>";
+            	    echo htmlspecialchars($_GET['error']);
+            	echo "</p>";
+            }
+	    ?>
+          <input type="file" name="images[]" multiple id="">
+          <button type="submit" name= "upload">Upload</button>
+</form>    
+
+
+<?php if ($result->num_rows > 0) { ?>
+	<div class="gallery">
+		<h4>All Images</h4>
+		<?php foreach ($images as $image) { ?>
+		   <img src="uploads/<?=$image['img_name']?>">
+		<?php } ?>
+	</div>
+	<?php } ?>
+
+</body>
+</html>
---
+++ image_upload/upload.php
@@ -0,0 +1,95 @@
+<?php
+
+if (isset($_POST['upload'])) {
+    require_once '../connectionSetup.php';
+    $images = $_FILES['images'];
+
+    $num_of_imgs = count($images['name']);
+    echo $num_of_imgs;
+
+    for ($i = 0; $i < $num_of_imgs; $i++) {
+
+        # get the image info and store them in var
+        $image_name = $images['name'][$i];
+        $tmp_name   = $images['tmp_name'][$i];
+        $error      = $images['error'][$i];
+
+        # if there is not error occurred while uploading
+        if ($error === 0) {
+            # get image extension store it in var
+            $img_ex = pathinfo($image_name, PATHINFO_EXTENSION);
+
+    		/** 
+			convert the image extension into lower case 
+			and store it in var 
+			**/
+			$img_ex_lc = strtolower($img_ex);
+
+
+
+                        /** 
+			crating array that stores allowed
+			to upload image extensions.
+			**/
+			$allowed_exs = array('jpg', 'jpeg', 'png');
+
+
+			/** 
+			check if the the image extension 
+			is present in $allowed_exs array
+			**/
+
+
+
+			if (in_array($img_ex_lc, $allowed_exs)) {
+				/** 
+				 renaming the image name with 
+				 with random string
+				**/
+				$new_img_name = uniqid('IMG-', true).'.'.$img_ex_lc;
+
+
+                
+                # crating upload path on root directory
+                $img_upload_path = 'uploads/'.$new_img_name;
+
+                # inserting imge name into database
+				$sql  = "INSERT INTO college_images (clz_id, img_name) VALUES (?, ?)";
+				$stmt = $conn->prepare($sql);
+				$stmt->execute([$i,$new_img_name]);
+				
+
+                # move uploaded image to 'uploads' folder
+                move_uploaded_file($tmp_name, $img_upload_path);
+
+		    	# redirect to 'index.php'
+	            header("Location: abc.php");
+
+
+			}else {
+				# error message
+    	     	$em = "You can't upload files of this type";
+
+	    		/*
+		    	redirect to 'index.php' and 
+		    	passing the error message
+		        */
+
+	            header("Location: abc.php?error=$em");
+			}
+
+
+
+        } else {
+            # error message
+            $em = "Unknown Error Occurred while uploading";
+
+            /*
+	    	redirect to 'index.php' and 
+	    	passing the error message
+	        */
+
+            header("Location: abc.php?error=$em");
+        }
+    }
+}
