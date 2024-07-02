749a9a2b296e0a254a8883aa4fe4df243ef63de6
Sushan Poudel <sushanpoudel75@gmail.com>
2024-07-03 00:16:56 +0545

    virtual tour data pulled from db

--- dashboard_student/virtualTour.php
+++ dashboard_student/virtualTour.php
@@ -2,11 +2,13 @@
 @require_once '../connectionSetup.php';
 session_start();
 if (!isset($_SESSION['std_id'])) {
-    header('location:../loginpage.php');
-    die();
-  }
+  header('location:../loginpage.php');
+  die();
+}
 
 $std_id = $_SESSION['std_id'];
+$sql = "SELECT * FROM college_info where Status = 1";
+$college = $conn->query($sql);
 
 ?>
 
@@ -35,18 +37,20 @@ $std_id = $_SESSION['std_id'];
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Rubik:wght@300;400;600;900&family=Work+Sans:wght@300;400;500;600;800&display=swap" rel="stylesheet">
- 
+
 
 </head>
 <style>
-    a:hover{
-        text-decoration: none;
-        color: black;
-    }
-    li{
-        width: 165px;
-    }
+  a:hover {
+    text-decoration: none;
+    color: black;
+  }
+
+  li {
+    width: 165px;
+  }
 </style>
+
 <body style="background-color: rgb(173, 255, 255);">
 
   <div class="courses page d-flex">
@@ -67,13 +71,56 @@ $std_id = $_SESSION['std_id'];
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
           </ol>
           <div class="carousel-inner" role="listbox">
-            <!-- Slide One - Set the background image for this slide in the line below -->
-            <div class="carousel-item active" style="background-image: url('../virtual_tour_file/amarsingh.jpeg')">
-              <div class="carousel-caption d-none d-md-block">
-                <h3>First Slide</h3>
-                <p>This is a description for the first slide.</p>
-              </div>
-            </div>
+
+            <?php
+            $i = 1;
+            while ($colleges_row = mysqli_fetch_assoc($college)) {
+              $clz_id = $colleges_row['clz_id'];
+              $sql20 = "SELECT * FROM `college_main_images` where `clz_id` = '$clz_id'";
+              $clz_images = $conn->query($sql20);
+              if ($clz_images->num_rows > 0) {
+
+                $clz_main_img = mysqli_fetch_assoc($clz_images);
+                $clz_logo = $clz_main_img['logo'];
+                $clz_img = $clz_main_img['main_img'];
+              } else {
+                $clz_logo = 'default.jpg';
+                $clz_img = 'default.jpg';
+              }
+
+
+
+            ?>
+              <?php
+              if ($i == 1) {
+
+
+              ?>
+                <!-- Slide One - Set the background image for this slide in the line below -->
+                <div class="carousel-item active" style="background-image: url('../image_upload/clz_main/<?php echo $clz_img; ?>')">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h3><?php echo $colleges_row['name']; ?></h3>
+                    <p><?php echo $colleges_row['description']; ?></p>
+                  </div>
+                </div>
+              <?php
+              }else {
+              ?>
+
+                <!-- Slide One - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url('../image_upload/clz_main/<?php echo $clz_img; ?>')">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h3><?php echo $colleges_row['name']; ?></h3>
+                    <p><?php echo $colleges_row['description']; ?></p>
+                  </div>
+                </div>
+              <?php
+              }
+              ?>
+            <?php
+            $i++;
+            }
+            ?>
             <!-- Slide Two - Set the background image for this slide in the line below -->
             <div class="carousel-item" style="background-image: url('../virtual_tour_file/college.jpg')">
               <div class="carousel-caption d-none d-md-block">
@@ -95,6 +142,7 @@ $std_id = $_SESSION['std_id'];
                 <p>This is a description for the Forth slide.</p>
               </div>
             </div>
+
 
 
           </div>
